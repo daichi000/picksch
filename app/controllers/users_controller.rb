5017522f190b174a6b733a5bf8839dd91953d40e
min-0000 <bigdogdag13@gmail.com>
2019-11-18 01:10:31 +0900

    いいね機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -75,6 +75,11 @@ class UsersController < ApplicationController
     redirect_to("/login")
   end
 
+  def likes
+    @user = User.find_by(id: params[:id])
+    @likes = Like.where(user_id: @user.id)
+  end
+
   def ensure_correct_user
     if @current_user.id != params[:id].to_i
       flash[:notice] ="権限がありません"
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
+  post 'likes/:post_id/create', to: 'likes#create'
+  post 'likes/:post_id/destroy', to: 'likes#destroy'
 
-  get 'login', to: 'users#login_form'
-  post 'login', to: 'users#login'
-  get 'logout', to: 'users#logout'
+ 
+  
   post "users/:id/update", to: "users#update"
   get "users/:id/edit", to: "users#edit"
   post "users/create", to: "users#create"
   get "signup", to: "users#new"
   get "users/index", to: "users#index"
   get "users/:id", to: "users#show"
-
+  post 'login', to: 'users#login'
+  get 'logout', to: 'users#logout'
+  get 'login', to: 'users#login_form'
+  get "users/:id/likes" => "users#likes"
   get "posts/index", to: "posts#index"
   get "posts/new", to: "posts#new"
   get "posts/:id", to: "posts#show"
